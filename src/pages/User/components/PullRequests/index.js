2610606c066b8ccfa906a4fb8c6120d3fec8f3ec
Ram Bhandari <rambhandari@lftechnology.com>
2019-10-22 23:29:09 +0545

    Typo fixes

--- src/pages/User/components/PullRequests/index.js
+++ src/pages/User/components/PullRequests/index.js
@@ -164,7 +164,7 @@ class PullRequests extends Component {
   /**
    *
    * @param {*} data
-   * @returns ValidPullRequests.
+   * @returns validPullRequests.
    */
   getValidPullRequests(data) {
     const validPullRequests = data.items.filter(pr => {
