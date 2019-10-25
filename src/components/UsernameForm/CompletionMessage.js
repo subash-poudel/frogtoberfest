5d0a2cff295ec5793f1b7b58ef1935f9a7c2598c
Saugat Acharya <mesaugat@gmail.com>
2019-10-25 13:22:02 +0545

    Text changes

--- src/components/UsernameForm/CompletionMessage.js
+++ src/components/UsernameForm/CompletionMessage.js
@@ -3,7 +3,6 @@ import React from 'react';
 const CompletionMessage = () => {
   return (
     <div className="text-center text-green-dark pb-2">
-      <p className="text-2xl">Thank you for your contributions.</p>
       <p className="text-xl">You have successfully completed Frogtoberfest 2019!</p>
     </div>
   );
--- src/pages/User/components/PullRequests/UserInfo/MotivationalMessage/index.js
+++ src/pages/User/components/PullRequests/UserInfo/MotivationalMessage/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import getMessage from './getMessage';
 
 const MotivationalMessage = ({ pullRequestCount, otherReposCount }) => (
-  <p className="font-medium text-green-darker text-3xl m-3">{getMessage(pullRequestCount, otherReposCount)}</p>
+  <p className="font-medium text-green-darker text-xl m-3">{getMessage(pullRequestCount, otherReposCount)}</p>
 );
 
 MotivationalMessage.propTypes = {
