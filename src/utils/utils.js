fb9f4da5263b3e7e7930798f40e23fb04fac7533
Ram Bhandari <rambhandari@lftechnology.com>
2019-10-26 13:29:23 +0545

    PR review fixes.

---
+++ src/utils/utils.js
@@ -0,0 +1,30 @@
+/**
+ * Returns formatted date eg Wed Jul 28 1993.
+ * 
+ * @param {date} dateTime  
+ * @returns {date}
+ */
+export function dateFormatter(dateTime) {
+  const date = new Date(dateTime);
+
+  return date.toDateString();
+}
+
+/**
+ * Fetch and return responses from Github apis.
+ * 
+ * @param {*} url 
+ * @param {*} GITHUB_TOKEN  
+ * @returns {Promise}
+ */
+export function fetchInfoFromGitHub(url, GITHUB_TOKEN) {
+  const response = fetch(url, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  })
+    .then(response => response)
+    .catch(error => error);
+
+  return response;
+}
