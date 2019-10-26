d0583f848bbcfd7dedede65c6fca23ae6f57e03c
Saugat Acharya <mesaugat@gmail.com>
2019-10-26 15:32:11 +0545

    Add new line after const

--- src/utils/utils.spec.js
+++ src/utils/utils.spec.js
@@ -5,36 +5,43 @@ describe('formatDate', () => {
 
   it('should return formatted date for given date in "dd-mmm-yyyy hh:mm" format.', () => {
     const date = '26-OCT-2019 13:48';
+    
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
   it('should return formatted date for given date in "dd-mmm-yy" format.', () => {
     const date = '26-OCT-19';
+    
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
   it('should return formatted date for given date in "dd-mmm-yyyy" format.', () => {
     const date = '26-OCT-2019';
+    
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
   it('should return formatted date for given date in common date format.', () => {
     const date = 'October 26, 2019 13:48:15';
+    
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
   it('should return formatted date for given date in another common date format.', () => {
     const date = '2019-10-26T03:24:00';
+    
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
-  const date = 'October Invalid';
-
   it('should not format the invalid date.', () => {
+    const date = 'October Invalid';
+    
     expect(formatDate(date)).not.toEqual(expectedDateString);
   });
 
   it('should respond with Invalid Date for invalid date.', () => {
+    const date = 'October Invalid';
+    
     expect(formatDate(date)).toEqual('Invalid Date');
   });
 });
