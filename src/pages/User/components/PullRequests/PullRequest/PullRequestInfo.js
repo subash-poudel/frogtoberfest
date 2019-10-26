fc166565470edacc1706a6de7e469954e61fb1a0
sagunji <karanjit.sagun01@gmail.com>
2019-10-26 13:58:38 +0545

    Open PR links in new tab

--- src/pages/User/components/PullRequests/PullRequest/PullRequestInfo.js
+++ src/pages/User/components/PullRequests/PullRequest/PullRequestInfo.js
@@ -9,7 +9,12 @@ const PullRequestInfo = ({ pullRequest }) => (
         {pullRequest.user.login}
       </a>{' '}
       submitted a pull request{' '}
-      <a className="text-blue-dark link no-underline hover:underline" href={pullRequest.html_url}>
+      <a
+        className="text-blue-dark link no-underline hover:underline"
+        target="_blank"
+        rel="noopener noreferrer"
+        href={pullRequest.html_url}
+      >
         {pullRequest.repository_url.split('repos/')[1]}#{pullRequest.number}
       </a>
     </div>
