57769ebdf3367eab406d61dc7ceb1038babb28aa
Bipin Manandhar <kubipin07@gmail.com>
2019-10-26 15:39:38 +0545

    Add yarn test in travis

--- src/utils/utils.spec.js
+++ src/utils/utils.spec.js
@@ -3,27 +3,27 @@ import { formatDate } from './utils';
 describe('formatDate', () => {
   const expectedDateString = 'Sat Oct 26 2019';
 
-  it('should return formatted date for given date in "dd-mmm-yyyy hh:mm" format.', () => {
+  it('should return formatted date for a given date in "dd-mmm-yyyy hh:mm" format.', () => {
     const date = '26-OCT-2019 13:48';
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
-  it('should return formatted date for given date in "dd-mmm-yy" format.', () => {
+  it('should return formatted date for a given date in "dd-mmm-yy" format.', () => {
     const date = '26-OCT-19';
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
-  it('should return formatted date for given date in "dd-mmm-yyyy" format.', () => {
+  it('should return formatted date for a given date in "dd-mmm-yyyy" format.', () => {
     const date = '26-OCT-2019';
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
-  it('should return formatted date for given date in common date format.', () => {
+  it('should return formatted date for a given date in common date format.', () => {
     const date = 'October 26, 2019 13:48:15';
     expect(formatDate(date)).toEqual(expectedDateString);
   });
 
-  it('should return formatted date for given date in another common date format.', () => {
+  it('should return formatted date for a given date in another common date format.', () => {
     const date = '2019-10-26T03:24:00';
     expect(formatDate(date)).toEqual(expectedDateString);
   });
