286255d655900f64b6c019eeaa97e03b1bdebc21
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-10-29 17:29:01 +0545

    Revert "Enhance user experience on clicking 'this is me' button"

--- src/pages/User/components/PullRequests/MeLinkInfo.js
+++ src/pages/User/components/PullRequests/MeLinkInfo.js
@@ -10,13 +10,10 @@ class MeLinkInfo extends Component {
     username: PropTypes.string.isRequired
   };
 
-  storeUsernameAsMe = () => {
-    localStorage.setItem('myGithub', this.props.username);
-    this.forceUpdate();
-  };
+  storeUsernameAsMe = () => localStorage.setItem('myGithub', this.props.username);
 
-  render = () => {
-    let storeUsernameBtn = (
+  render = () => (
+    <div className="rounded mx-auto mt-8 overflow-hidden w-5/6 lg:w-1/2 mt-4">
       <button
         className="bg-blue-light text-blue-darker mx-auto mt-2 h-8 border-none pointer rounded-sm px-4 block saveUser"
         onClick={this.storeUsernameAsMe}
@@ -24,8 +21,6 @@ class MeLinkInfo extends Component {
       >
         This is Me
       </button>
-    );
-    let infoStr = (
       <p className="text-grey-dark mx-auto text-center my-4">
         In the future, you can find your PRs by visiting{' '}
         <a href={`${HOSTNAME}/me`} className="link text-orange underline-hover saveUser" id="melink">
@@ -34,30 +29,8 @@ class MeLinkInfo extends Component {
         </a>{' '}
         on this device.
       </p>
-    );
-    const savedUsername = localStorage.getItem('myGithub');
-
-    if (savedUsername === this.props.username) {
-      storeUsernameBtn = null;
-      infoStr = (
-        <p className="text-mid-grey mx-auto text-center my-4">
-          Username {this.props.username} saved! You can visit{' '}
-          <a href={`${HOSTNAME}/me`} className="link saveUser" id="melink">
-            {HOSTNAME}
-            /me
-          </a>{' '}
-          now!
-        </p>
-      );
-    }
-
-    return (
-      <div className="rounded mx-auto mt-8 overflow-hidden w-5/6 lg:w-1/2 mt-4">
-        {storeUsernameBtn}
-        {infoStr}
-      </div>
-    );
-  };
+    </div>
+  );
 }
 
 const buttonStyle = {
